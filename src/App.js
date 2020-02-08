3a66639dd45d024e36844831692ce59af6a601ac
Cory Fladhammer <cjfladham@hotmail.com>
2020-02-07 20:32:40 -0600

    whoa boy

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import HelloWorld from './state-drills/HelloWorld'
+import Bomb from './state-drills/Bomb'
+import RouletteGun from './state-drills/RouletteGun'
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <br />
+      <Bomb />
+      <br />
+      <RouletteGun />
+    </div>
+  )
+}
+
+export default App
