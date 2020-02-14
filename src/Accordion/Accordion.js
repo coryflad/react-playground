77344dadca3db20e09c96cbee2e90771b0768f2b
Cory Fladhammer <cjfladham@hotmail.com>
2020-02-13 21:10:39 -0600

    added shopping list

--- src/Accordion/Accordion.js
+++ src/Accordion/Accordion.js
@@ -2,37 +2,37 @@ import React, { Component } from 'react'
 import './Accordion.css'
 
 export default class Accordion extends Component {
-    static defaultProps = { sections: [] };
+  static defaultProps = {sections: []};
 
-    state = { activeSectionIndex: null, }
+  state = {activeSectionIndex: null,}
 
-    handleSetActiveSecion = (sectionIndex) => {
-        this.setState({ activeSectionIndex: sectionIndex })
-    }
+  handleSetActiveSecion = (sectionIndex) => {
+    this.setState({ activeSectionIndex: sectionIndex })
+  }
 
-    renderItem(section, index, activeSectionIndex) {
-        return (
-            <li className='Accordion__item' key={index}>
-                <button
-                    type='button'
-                    onClick={() => this.handleSetActiveSecion(index)}
-                >
-                    {section.title}
-                </button>
-                {(activeSectionIndex === index) && <p>{section.content}</p>}
-            </li>
-        )
-    }
+  renderItem(section, index, activeSectionIndex) {
+    return (
+      <li className='Accordion__item' key={index}>
+        <button
+          type='button'
+          onClick={() => this.handleSetActiveSecion(index)}
+        >
+          {section.title}
+        </button>
+        {(activeSectionIndex === index) && <p>{section.content}</p>}
+      </li>
+    )
+  }
 
-    render() {
-        const { activeSectionIndex } = this.state
-        const { sections } = this.props
-        return (
-            <ul className='Accordion'>
-                {sections.map((section, idx) =>
-                    this.renderItem(section, idx, activeSectionIndex)
-                )}
-            </ul>
-        )
-    }
+  render() {
+    const { activeSectionIndex } = this.state
+    const { sections } = this.props
+    return (
+      <ul className='Accordion'>
+        {sections.map((section, idx) =>
+          this.renderItem(section, idx, activeSectionIndex)
+        )}
+      </ul>
+    )
+  }
 }
--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from "react";
-import Accordion from './Accordion/Accordion';
-
-// array of objects each with a name and content
-const sectionsProp = [
-  {
-    title: 'Section 1',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-  },
-  {
-    title: 'Section 2',
-    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
-  },
-  {
-    title: 'Section 3',
-    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
-  },
-];
+import AddItemForm from './Shopping-list/AddItemForm';
+import ShoppingList from './Shopping-list/ShoppingList';
 
 class App extends React.Component {
+  state = {
+    shoppingItems: [
+      /* put stub items in here for testing */
+      { name: 'apples', checked: false },
+      { name: 'oranges', checked: true },
+      { name: 'bread', checked: false },
+    ]
+  };
+
+  handleDeleteItem = (item) => {
+    const newItems = this.state.shoppingItems.filter(itm => itm !== item)
+    this.setState({
+      shoppingItems: newItems
+    })
+  }
+  handleCheckItem = (item) => {
+    const newItems = this.state.shoppingItems.map(itm => {
+      if (itm === item) {
+        itm.checked = !itm.checked
+      }
+      return itm
+    })
+    this.setState({
+      shoppingItems: newItems
+    })
+  }
+
+  handleAddItem = (itemName) => {
+    const newItems = [
+      ...this.state.shoppingItems,
+      { name: itemName, checked: false }
+    ]
+    this.setState({
+      shoppingItems: newItems
+    })
+  }
+
   render() {
     return (
-      <Accordion sections={sectionsProp} />
+      <>
+        <header>
+          <h1>Shopping List</h1>
+        </header>
+        <main>
+          <section>
+            <AddItemForm
+              onAddItem={this.handleAddItem}
+            />
+          </section>
+          <section>
+            <ShoppingList
+              items={this.state.shoppingItems}
+              onDeleteItem={this.handleDeleteItem}
+              onCheckItem={this.handleCheckItem}
+            />
+          </section>
+        </main>
+      </>
     )
   }
 }
